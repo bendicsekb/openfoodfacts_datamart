c89b5fc3cc6f718fc4ba6e0663c5f9b87f044a49
Maroufd <maroufdani@gmail.com>
2022-09-28 18:39:35 +0200

    Add files via upload
    create statment

--- base_tables.sql
+++ base_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `DIM_CONTRIBUTOR` (
-  `nb_seq_contib` int,
+  `num_seq_contib` int,
   `pseudo` varchar(1000),
-  PRIMARY KEY (`nb_seq_contib`)
+  PRIMARY KEY (`num_seq_contib`)
 );
 
 CREATE TABLE `DIM_PNNS` (
@@ -11,16 +11,16 @@ CREATE TABLE `DIM_PNNS` (
 );
 
 CREATE TABLE `DIM_DATE` (
-  `nb_seq_date` int,
+  `num_seq_date` int,
   `dim_date` date,
-  PRIMARY KEY (`Nb_seq_date`)
+  PRIMARY KEY (`num_seq_date`)
 );
 
 CREATE TABLE `DIM_NUTRITION_SCORE` (
-  `nb_seq_ns` int,
+  `num_seq_ns` int,
   `nutrition_score` int,
   `nutrition_cat` varchar(1000),
-  PRIMARY KEY (`nb_seq_ns`)
+  PRIMARY KEY (`num_seq_ns`)
 );
 
 CREATE TABLE `DIM_BARCODE` (
@@ -30,21 +30,25 @@ CREATE TABLE `DIM_BARCODE` (
 );
 CREATE TABLE `PRODUCT_FACTS` (
   `num_seq_product` int,
-  `nb_seq_date` int,
+  `num_seq_date` int,
   `barcode` int,
-  `nb_seq_contib` int,
+  `num_seq_ns` int,
+  `num_seq_contib` int,
   PRIMARY KEY (`num_seq_product`),
-  CONSTRAINT date_const FOREIGN KEY (nb_seq_date) REFERENCES DIM_DATE(nb_seq_date),
+  CONSTRAINT date_const FOREIGN KEY (num_seq_date) REFERENCES DIM_DATE(num_seq_date),
   CONSTRAINT barcode_const FOREIGN KEY (barcode) REFERENCES DIM_BARCODE(barcode),
-  CONSTRAINT nb_seq_contib_const FOREIGN KEY (nb_seq_contib) REFERENCES DIM_CONTRIBUTOR(nb_seq_contib)
+  CONSTRAINT num_seq_contib_const FOREIGN KEY (num_seq_contib) REFERENCES DIM_CONTRIBUTOR(num_seq_contib),
+  CONSTRAINT num_seq_ns_const FOREIGN KEY (num_seq_ns) REFERENCES DIM_NUTRITION_SCORE(num_seq_ns)
 );
 CREATE TABLE `PRODUCT_VERSION_FACTS` (
   `num_seq_version` int,
-  `nb_seq_date` int,
-  `Name_PNNS2` int,
-  `nb_seq_contib` int,
+  `num_seq_date` int,
+  `name_PNNS2` int,
+  `num_seq_ns` int,
+  `num_seq_contib` int,
   PRIMARY KEY (`num_seq_version`),
-  CONSTRAINT date_const FOREIGN KEY (nb_seq_date) REFERENCES DIM_DATE(nb_seq_date),
-  CONSTRAINT Name_PNNS2_const FOREIGN KEY (Name_PNNS2) REFERENCES DIM_PNNS(Name_PNNS2),
-  CONSTRAINT nb_seq_contib_const FOREIGN KEY (nb_seq_contib) REFERENCES DIM_CONTRIBUTOR(nb_seq_contib)
+  CONSTRAINT date_const_version FOREIGN KEY (num_seq_date) REFERENCES DIM_DATE(num_seq_date),
+  CONSTRAINT name_PNNS2_const_version FOREIGN KEY (name_PNNS2) REFERENCES DIM_PNNS(name_PNNS2),
+  CONSTRAINT num_seq_contib_const_version FOREIGN KEY (num_seq_contib) REFERENCES DIM_CONTRIBUTOR(num_seq_contib),
+  CONSTRAINT num_seq_ns_const_version FOREIGN KEY (num_seq_ns) REFERENCES DIM_NUTRITION_SCORE(num_seq_ns)
 );
